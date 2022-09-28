8f80e4419f6cdf2033b8215f7505bb68b17ce761
Arief Setiyo Nugroho <ayiexz22@gmail.com>
2022-09-28 10:53:24 +0700

    update readme

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ bun install
 To run:
 
 ```bash
-bun run index.ts
+bun run src/index.ts
+```
+
+With autoreload:
+
+```bash
+bun run nodemon
 ```
 
 This project was created using `bun init` in bun v0.1.8. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { env } from "process"
+import { colourText } from "./helper"
 import { router } from "./router"
 
+const port = parseInt(env.PORT ?? '4000')
+
+console.log("\n", colourText("FgCyan", "Server started on port: " + port), "\n")
+
 Bun.serve({
-  port: parseInt(env.PORT ?? '4000'),
-  fetch: router.fetch
+  port,
+  fetch: router.fetch,
 })
