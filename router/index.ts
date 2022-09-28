8f3f0fc48f6cb452882694ed49d2977914ca4b16
Arief Setiyo Nugroho <ayiexz22@gmail.com>
2022-09-28 08:38:21 +0700

    add nodemon, move to src

--- router/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { health } from "./root";
-import { example } from "./example";
-
-export const router = new Hono()
-
-router.get('/', health);
-router.route('/example', example)
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
+import { cors } from 'hono/cors'
+import { health } from "./root";
+import { example } from "./example";
+
+export const router = new Hono()
+
+router.use('*', logger())
+router.use('*', prettyJSON())
+router.use('*', cors())
+
+router.get('/', health);
+router.route('/example', example)
